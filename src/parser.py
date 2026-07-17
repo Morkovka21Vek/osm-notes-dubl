93bb0eb46776bec5575302d5caf942be6457ed08
Morkovka21Vek <morkovka21vek@gmail.com>
2026-07-17 10:51:13 +0300

    feat: add uid to username list

--- src/parser.py
+++ src/parser.py
@@ -1,7 +1,10 @@
 import xml.etree.ElementTree as ET
+import json
 import struct
 
 record = struct.Struct("<IiiBI") # if id or uid > 2^32-1 use Q
+
+users = {}
 
 with open("notes.bin", "wb") as out:
     for event, elem in ET.iterparse("planet-notes-latest.osn", events=("end",)):
@@ -10,6 +13,7 @@ with open("notes.bin", "wb") as out:
             for comment in elem.findall("comment"):
                 if comment.attrib.get("action") == "closed":
                     last_closer_uid = comment.attrib["uid"]
+                    users[comment.attrib["uid"]] = comment.attrib["user"]
 
             out.write(record.pack(
                 int(elem.attrib["id"]),
@@ -20,3 +24,7 @@ with open("notes.bin", "wb") as out:
             ))
 
             elem.clear()
+
+with open("users.json", "w", encoding="utf-8") as file:
+    json.dump(users, file, ensure_ascii=False, separators=(",", ":"))
+
